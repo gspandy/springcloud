0827772821acf82b61ee08d2a34692b563e5483b
zrdDemon <469693055@qq.co>
2018-01-18 14:41:35 +0800

    config配置文件

--- springCloudConfigServer/src/main/java/com/guandou/conf/Application.java
+++ springCloudConfigServer/src/main/java/com/guandou/conf/Application.java
@@ -1,8 +1,11 @@
 package com.guandou.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
 /**
  * Spring could conf程序主入口
@@ -12,6 +15,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
+
+
 	public static void main(String[] args) {   
         SpringApplication.run(Application.class,args);   
     }   
